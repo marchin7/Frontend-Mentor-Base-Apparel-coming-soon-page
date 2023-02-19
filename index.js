2ddc5b9cf4ff5303ce38aee7626e87be456d0ef3
marchin7 <114770259+marchin7@users.noreply.github.com>
2023-02-19 20:59:08 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-const form = document.getElementById('form');
-const input = document.getElementById('input');
-const errorIcon = document.getElementById('errorIcon');
-const submitBtn = document.getElementById('submitBtn');
-const submitMessage = document.getElementById('submitMessage');
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const errorIcon = document.getElementById("errorIcon");
+const submitBtn = document.getElementById("submitBtn");
+const submitMessage = document.getElementById("submitMessage");
 const regExp = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
-form.addEventListener('submit', (e)=>{
+form.addEventListener("submit", (e) => {
     e.preventDefault();
 });
 
-function removeError(value){
-    if(value === '' || value.match(regExp)){
-        submitMessage.style.opacity = '0';
-        errorIcon.style.opacity = '0';
-    } 
-};
+function removeError(value) {
+    if (value === "" || value.match(regExp)) {
+        submitMessage.style.opacity = "0";
+        errorIcon.style.opacity = "0";
+    }
+}
 
-submitBtn.addEventListener('click', ()=>{
-    if(input.value === '') {
-        submitMessage.innerText = 'This field is required';
-        submitMessage.style.opacity = '1';
-        errorIcon.style.opacity = '1';
-        submitMessage.style.color = 'red';
+submitBtn.addEventListener("click", () => {
+    if (input.value === "") {
+        submitMessage.innerText = "This field is required";
+        submitMessage.style.opacity = "1";
+        errorIcon.style.opacity = "1";
+        submitMessage.style.color = "red";
         input.focus();
-    }
-    else if(!input.value.match(regExp)){
-        submitMessage.innerText ='Please provide a valid email';
-        submitMessage.style.opacity = '1';
-        submitMessage.style.color ='red';
-        errorIcon.style.opacity = '1';
+    } else if (!input.value.match(regExp)) {
+        submitMessage.innerText = "Please provide a valid email";
+        submitMessage.style.opacity = "1";
+        submitMessage.style.color = "red";
+        errorIcon.style.opacity = "1";
         input.focus();
-    }
-    else{
-        submitMessage.innerText = 'Thank you for submiting your email';
-        submitMessage.style.opacity = '1';
-        submitMessage.style.color ='green';
-        errorIcon.style.opacity = '0';
-        input.value = '';
-        setTimeout(()=>{submitMessage.style.opacity = '0'},2000);
+    } else {
+        submitMessage.innerText = "Thank you for submiting your email";
+        submitMessage.style.opacity = "1";
+        submitMessage.style.color = "green";
+        errorIcon.style.opacity = "0";
+        input.value = "";
+        setTimeout(() => {
+            submitMessage.style.opacity = "0";
+        }, 2000);
     }
 });
