0bd0f8381539b928cfd6688db330b8b22dc7fafc
marchin7 <114770259+marchin7@users.noreply.github.com>
2023-02-27 18:37:48 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -8,39 +8,39 @@ const regExp = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     validateEmail();
+    console.log("wtf?");
 });
 
 function validateEmail() {
     if (input.value === "") {
         submitMessage.innerText = "This field is required";
-        submitMessage.classList.add("error");
-        errorIcon.classList.add("error");
-        input.classList.add("error");
-        submitMessage.classList.remove("success");
+        submitMessage.style.opacity = "1";
+        errorIcon.style.opacity = "1";
+        submitMessage.style.color = "red";
+        input.style.borderColor = "rgb(249, 98, 98)";
     } else if (!input.value.match(regExp)) {
         submitMessage.innerText = "Please provide a valid email";
-        submitMessage.classList.add("error");
-        errorIcon.classList.add("error");
-        input.classList.add("error");
-        submitMessage.classList.remove("success");
+        submitMessage.style.opacity = "1";
+        submitMessage.style.color = "red";
+        errorIcon.style.opacity = "1";
+        input.style.borderColor = "rgb(249, 98, 98)";
     } else {
         submitMessage.innerText = "Thank you for submiting your email";
-        errorIcon.classList.remove("error");
-        input.classList.remove("error");
-        submitMessage.classList.add("success");
+        submitMessage.style.opacity = "1";
+        submitMessage.style.color = "green";
+        errorIcon.style.opacity = "0";
+        input.style.borderColor = "rgba(206, 151, 151, 0.5)";
         input.value = "";
         setTimeout(() => {
-            submitMessage.classList.remove("success");
+            submitMessage.style.opacity = "0";
         }, 2000);
     }
 }
 
 input.addEventListener("input", () => {
     if (input.value === "" || input.value.match(regExp)) {
-        submitMessage.classList.remove("error");
-        errorIcon.classList.remove("error");
-        input.classList.remove("error");
+        submitMessage.style.opacity = "0";
+        errorIcon.style.opacity = "0";
+        input.style.borderColor = "rgba(206, 151, 151, 0.5)";
     }
 });
-
-
